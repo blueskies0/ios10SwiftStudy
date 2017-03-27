d8679db317b55d15e661283d91c45fa27f423779
Yin <yin@mac-baseimage-10.local>
2017-03-27 23:49:15 +0800

    Initial Commit

---
+++ FaceView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceView
+//
+//  Created by Yin on 3/27/17.
+//  Copyright © 2017 Yin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
