0eb61b8a04026ee0c6489f626bc24688206869de
Yin <yin@mac-baseimage-10.local>
2017-04-20 16:33:34 +0800

    Practise of L5:Adding controller to face view

--- FaceView/ViewController.swift
+++ FaceView/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var faceView: FaceView!{
+        // didSet will be called when ios hooked up this outlet to the FaceView.
+        didSet{
+            updateUI()
+            print("faceView is changed")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var expression = FacialExpression(eyes:.open,mouth:.grin)
+    {
+        // didSet will be called when the expression is changed.
+        didSet{
+            updateUI()
+            print("expression is changed")
+        }
     }
+    
+    private func updateUI(){
+       
+        switch expression.eyes
+        {
+        case .open:
+            // To prevent the case when the change of expression
+            // happened before the FaceView is hooked, append ? to faceView.
+            // As a result, instead of crashing, it will be ignore when faceView is nil.
+            faceView?.eyeOpen = true
+        case .closed:
+            faceView?.eyeOpen = false
+        case .squinting:
+            faceView?.eyeOpen = false
+        }
+        // usage of ??  : 
+        // If the parameter comes before ?? is not nil, use its value directly,
+        // otherwise use the value following the ??
+        faceView?.mouthCurvature = mouthCurvature[expression.mouth] ?? 1.0
 
-
+    }
+    private var mouthCurvature:[FacialExpression.Mouth:CGFloat] = [FacialExpression.Mouth.frown: -1.0, .smirk : -0.5, .neutral : 0.0, .grin : 0.5, .smile : 1.0]
 }
-
